13814c0d80fd7e10d5f8fc0f629057f5af55069b
kathleendiep <kathleendiep@gmail.com>
2022-04-21 19:44:39 -0700

    migrate name change

--- posts_api/models.py
+++ posts_api/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    Name = models.CharField(max_length=32)
-    Category = models.CharField(max_length=32)
-    Breed = models.CharField(max_length=32)
-    Description = models.CharField(max_length=200)
-    City = models.CharField(max_length=50)
-    State = models.CharField(max_length=50)
+    name = models.CharField(max_length=32)
+    category = models.CharField(max_length=32)
+    breed = models.CharField(max_length=32)
+    description = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
     img = models.CharField(max_length=2000)
