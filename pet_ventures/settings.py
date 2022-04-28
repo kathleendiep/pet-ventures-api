4a0e02ff53baccf5cf91786eec5c63a1713bf7b6
kathleendiep <kathleendiep@gmail.com>
2022-04-27 21:46:46 -0700

    authenticated

--- pet_ventures/settings.py
+++ pet_ventures/settings.py
@@ -66,9 +66,9 @@ REST_FRAMEWORK = {
     'rest_framework.authentication.SessionAuthentication',
     'rest_framework.authentication.TokenAuthentication',
     ],
-    # 'DEFAULT_PERMISSION_CLASSES': [
-    # 'rest_framework.permissions.AllowAny',
-    # ],
+    'DEFAULT_PERMISSION_CLASSES': [
+    'rest_framework.permissions.IsAuthenticated',
+    ],
 }
 
 # AUTH_USER_MODEL = 'profiles.User'
