bc42f8b277b866026909a992acfa3204f435dc44
kathleendiep <kathleendiep@gmail.com>
2022-04-29 15:23:26 -0700

    Added auth token to each item

--- posts_api/views.py
+++ posts_api/views.py
@@ -4,18 +4,24 @@ from .serializers import PostSerializer, UserSerializer
 from .models import Post
 from django.contrib.auth.models import User 
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import AllowAny, IsAuthenticatedOrReadOnly, IsAuthenticated
 
 # Create your views here - returns a web response from seralizer
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all().order_by('id') # tell django how to retrieve all objects from the DB, order by id ascending
     serializer_class = PostSerializer # tell django what serializer to use
-    # authentication_classes = [TokenAuthentication]
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all().order_by('id')
     serializer_class = PostSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 class UserViewSet(generics.ListCreateAPIView): 
     queryset = User.objects.all().order_by('id')
     serializer_class = UserSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     # authentication_classes = [TokenAuthentication]
